53575d2581dd717230dd48699a87f5440157b5d6
Sam <livaco@livaco.dev>
2025-08-11 09:12:29 +0100

    feat: backport to 1.21.1

--- src/main/java/dev/livaco/deathcharm/items/DeathCharmItem.java
+++ src/main/java/dev/livaco/deathcharm/items/DeathCharmItem.java
@@ -8,28 +8,25 @@ import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Rarity;
 import net.minecraft.world.item.TooltipFlag;
-import net.minecraft.world.item.component.TooltipDisplay;
 
-import java.util.function.Consumer;
+import java.util.List;
 
 public class DeathCharmItem extends Item {
     public DeathCharmItem(Properties properties) {
         super(properties.stacksTo(1)
-                .setNoCombineRepair()
+                .setNoRepair()
                 .component(DataComponentsRegistration.USES_LEFT.get(), new RemainingUsesComponent(Config.CHARM_USES.getAsInt()))
                 .rarity(Rarity.UNCOMMON));
     }
 
-    @SuppressWarnings("deprecation")
     @Override
-    public void appendHoverText(ItemStack stack, TooltipContext context, TooltipDisplay tooltipDisplay, Consumer<Component> tooltipAdder, TooltipFlag flag) {
-        // I'm aware this is deprecated, but I can't find any reference as to what your meant to do instead of this? Every 1.21.8 mod I could find uses this still
-        super.appendHoverText(stack, context, tooltipDisplay, tooltipAdder, flag);
+    public void appendHoverText(ItemStack stack, TooltipContext context, List<Component> tooltipComponents, TooltipFlag tooltipFlag) {
+        super.appendHoverText(stack, context, tooltipComponents, tooltipFlag);
 
         RemainingUsesComponent remaining = stack.get(DataComponentsRegistration.USES_LEFT.get());
         if(remaining != null && Config.CHARM_USES.getAsInt() != 0)
-            tooltipAdder.accept(Component.translatable("tooltip.deathcharm.deathcharm.usesleft", remaining.usesLeft()));
+            tooltipComponents.add(Component.translatable("tooltip.deathcharm.deathcharm.usesleft", remaining.usesLeft()));
 
-        tooltipAdder.accept(Component.translatable("tooltip.deathcharm.deathcharm.description"));
+        tooltipComponents.add(Component.translatable("tooltip.deathcharm.deathcharm.description"));
     }
 }
